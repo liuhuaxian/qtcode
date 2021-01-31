350425e8d9a4cc8a06229eff578a2aa67c792c2d
liuhuaxian <1016637467@qq.com>
2021-01-31 18:07:15 +0800

    2021年01月31日 18:07:14 push 11-code

--- push.sh
+++ push.sh
@@ -1,10 +1,19 @@
 #!/bin/sh
 set -x
+cleanother(){
+cd $1
+RMOBJ="*.pro.user *.Debug *.Release Makefile .qmake.stash debug release"
+find . -type d -name "build*-Debug" | xargs rm -fr
+for obj in $RMOBJ
+do
+	find . -type d -name "$obj" | xargs rm -fr 
+done
+cd ..
+}
+
 if [[ $1 != '' ]]
 then
-	cd $1
-	rm *.pro.user *.Debug *.Release Makefile .qmake.stash debug release -fr
-	cd ..
+	cleanother $1
 fi
 rm *MinGW_32bit-Debug -fr
 git add *
