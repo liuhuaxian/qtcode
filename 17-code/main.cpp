17900c38af80c585a49333b2080c861531f43edb
liuhuaxian <1016637467@qq.com>
2021-02-01 13:34:32 +0800

    2021年02月 1日 13:34:32 push 17-code

---
+++ 17-code/main.cpp
@@ -0,0 +1,27 @@
+#include <QApplication>
+#include <QWidget>
+#include <QDialog>
+#include <QDebug>
+#include "Dialog.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    Dialog dlg;
+    int r = dlg.exec();
+
+    if( r == QDialog::Accepted )
+    {
+        qDebug() << "Accepted";
+    }
+    else if( r == QDialog::Rejected )
+    {
+        qDebug() << "Rejected";
+    }
+    else
+    {
+        qDebug() << r;
+    }
+    
+    return r;
+}
