ebfae8a44570ede1acb260b273813e425cdd01ea
liuhuaxian <1016637467@qq.com>
2021-02-01 13:42:27 +0800

    2021年02月 1日 13:42:27 push 17-code

--- push.sh
+++ push.sh
@@ -5,11 +5,11 @@ RMOBJF="*.pro.user *.Debug *.Release Makefile .qmake.stash"
 RMOBJD="debug release build*-Debug"
 for file in $RMOBJF
 do
-	find .-type f -name "$file" | xargs rm -fr 
+	find . -type f -name "$file" | xargs rm -fr 
 done
 for dir in $RMOBJD
 do
-	find .-type d -name "$dir" | xargs rm -fr 
+	find . -type d -name "$dir" | xargs rm -fr 
 done
 }
 cleanother
