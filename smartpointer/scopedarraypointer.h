4f2e7cdce818bb293335b9b2df50caa132d175c6
liuhuaxian <1016637467@qq.com>
2021-02-22 22:00:30 +0800

    2021年02月22日 22:00:30 push smartpointer

--- smartpointer/scopedarraypointer.h
+++ smartpointer/scopedarraypointer.h
@@ -7,9 +7,14 @@
 template <typename T>
 class ScopedArrayPointer : public QScopedArrayPointer<T>
 {
+public:
     ScopedArrayPointer() : QScopedArrayPointer<T>()
     {
         qDebug() << "in ScopedArrayPointer()" ;
+    }
+    ScopedArrayPointer(T* p=Q_NULLPTR) : QScopedArrayPointer<T>(p)
+    {
+        qDebug() << "in ScopedArrayPointer(T* p=Q_NULLPTR)" ;
     }
     ~ScopedArrayPointer()
     {
