75dc4d5d41a83421ca74f13deb8830ef10c03836
liuhuaxian <1016637467@qq.com>
2021-02-24 16:08:53 +0800

    2021年02月24日 16:08:53 push 49-code

--- 48-code/NotePad/ReplaceDialog.cpp
+++ 48-code/NotePad/ReplaceDialog.cpp
@@ -40,13 +40,17 @@ void ReplaceDialog::onReplaceClicked()
 
     if( (m_pText != NULL) && (target != "") && (to != "") )
     {
+        //获取光标标识的文本内容
         QString selText = m_pText->textCursor().selectedText();
 
+        //判断光标标识的文本内容是否为target。
         if( selText == target )
         {
+            //将光标标识的内容替换为目标内容to
             m_pText->insertPlainText(to);
         }
 
+        //直接调用查找槽函数，继续查找下一个目标字符
         onFindClicked();
     }
 }
@@ -62,6 +66,7 @@ void ReplaceDialog::onReplaceAllClicked()
 
         text.replace(target, to, m_matchChkBx.isChecked() ? Qt::CaseSensitive : Qt::CaseInsensitive);
 
+        //先将文本主编辑框清空内容，再将更换好的文本内容填入对话框中。
         m_pText->clear();
 
         m_pText->insertPlainText(text);
