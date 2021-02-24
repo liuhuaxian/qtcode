6cb1df4427c31d16b4e78d7008413443fa04ee2d
liuhuaxian <1016637467@qq.com>
2021-02-24 17:35:06 +0800

    2021年02月24日 17:35:06 push 50-code

---
+++ 50-code/NotePad/MainWindow.h
@@ -0,0 +1,88 @@
+#ifndef _MAINWINDOW_H_
+#define _MAINWINDOW_H_
+
+#include <QMainWindow>
+#include <QKeySequence>
+#include <QMenuBar>
+#include <QToolBar>
+#include <QAction>
+#include <QPlainTextEdit>
+#include <QLabel>
+#include <QFileDialog>
+#include <QSharedPointer>
+#include "FindDialog.h"
+#include "ReplaceDialog.h"
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+private:
+    QPlainTextEdit mainEditor;
+    QLabel statusLbl;
+    QString m_filePath;
+    bool m_isTextChanged;
+    QSharedPointer<FindDialog> m_pFindDlg;
+    QSharedPointer<ReplaceDialog> m_pReplaceDlg;
+
+    MainWindow();
+    MainWindow(const MainWindow&);
+    MainWindow& operator= (const MainWindow&);
+
+    bool construct();
+    bool initMenuBar();
+    bool initToolBar();
+    bool initStatusBar();
+    bool initMainEditor();
+    bool initFileMenu(QMenuBar* mb);
+    bool initEditMenu(QMenuBar* mb);
+    bool initFormatMenu(QMenuBar* mb);
+    bool initViewMenu(QMenuBar* mb);
+    bool initHelpMenu(QMenuBar* mb);
+    bool initFileToolItem(QToolBar* tb);
+    bool initEditToolItem(QToolBar* tb);
+    bool initFormatToolItem(QToolBar* tb);
+    bool initViewToolItem(QToolBar* tb);
+    bool makeAction(QAction*& action, QWidget* parent, QString text, int key);
+    bool makeAction(QAction*& action, QWidget* parent, QString tip, QString icon);
+
+    QString showFileDialog(QFileDialog::AcceptMode mode, QString title);
+    int showQueryMessage(QString message);
+    void showErrorMessage(QString message);
+    QString saveCurrentData(QString path = "");
+    void preEditorChange();
+    void openFileToEditor(QString path);
+    QAction* findMenuBarAction(QString text);
+    QAction* findToolBarAction(QString text);
+protected:
+    void closeEvent(QCloseEvent* e);
+    void dragEnterEvent(QDragEnterEvent* e);
+    void dropEvent(QDropEvent* e);
+private slots:
+    void onFileNew();
+    void onFileOpen();
+    void onFileSave();
+    void onFileSaveAs();
+    void onFilePrint();
+    void onFileExit();
+    void onTextChanged();
+    void onCopyAvailable(bool available);
+    void onRedoAvailable(bool available);
+    void onUndoAvailable(bool available);
+    void onCursorPositionChanged();
+    void onEditFind();
+    void onEditReplace();
+    void onEditDelete();
+    void onEditGoto(); //行间跳转功能
+    void onFormatWrap(); //自动换行设置
+    void onFormatFont(); //设置文本编辑框的字体
+    void onViewToolBar();
+    void onViewStatusBar();
+    void onHelpManual(); //打开帮助页面
+    void onHelpAbout();  //打开关于对话框
+public:
+    static MainWindow* NewInstance();
+    ~MainWindow();
+};
+
+#endif // _MAINWINDOW_H_
